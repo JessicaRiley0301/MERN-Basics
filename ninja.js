e1d1ac7c4cb7bde8c009f543d869adb5dd0212f1
JessicaRiley0301 <82771069+JessicaRiley0301@users.noreply.github.com>
2021-11-20 11:59:52 -0800

    Add files via upload

---
+++ ninja.js
@@ -0,0 +1,20 @@
+class Ninja {
+    constructor(name, health = 10, speed = 3, strength = 3){
+        this.name = name; 
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName(){
+        console.log(this.name);
+
+    }
+    showStats(){
+        console.log("Name: " + this.name, "Strength: " + this.strength, " Speed: " + this.speed, " Health: " + this.health)
+    }
+    drinkSake(){
+        this.health += 10;
+        console.log(this.health);
+    }
+}
